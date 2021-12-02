4fdb865696e79fe966c1e641c1053672bd6ab0ac
Joseph Acevedo <josephacevedo1997@gmail.com>
2021-12-01 21:19:54 -0600

    logo

--- js/site.js
+++ js/site.js
@@ -21,7 +21,7 @@ function getValues() {
 // brains of the operation
 function fizzBuzz(fizzValue, buzzValue) {
   let returnArray = [];
-
+  // loop from 1 to 100 and find factors of fizz and buzz value
   for (let i = 1; i <= 100; i++) {
     if (i % fizzValue == 0 && i % buzzValue == 0) {
       returnArray.push("FizzBuzz");
@@ -35,6 +35,53 @@ function fizzBuzz(fizzValue, buzzValue) {
   }
   return returnArray;
 }
+// alternative with a switch statement. replace line 13 to see in action
+function fizzBuzzB(fizzValue, buzzValue) {
+  let returnArray = [];
+  let Fizz = false;
+  let Buzz = false;
+
+  for (let i = 1; i <= 100; i++) {
+    Fizz = i % fizzValue == 0;
+    Buzz = i % buzzValue == 0;
+
+    switch (true) {
+      case Fizz && Buzz:
+        {
+          returnArray.push("FizzBuzz");
+          break;
+        }
+      case Fizz:
+        {
+          returnArray.push("Fizz");
+          break;
+        }
+      case Buzz:
+        {
+          returnArray.push("Buzz");
+          break;
+        }
+      default: {
+        returnArray.push(i);
+        break;
+      }
+    }
+  }
+
+  return returnArray;
+}
+
+// another alternative using ternary replace line 13 to see in action
+function fizzBuzzC(fizzValue,buzzValue){
+  let returnArray = [];
+
+  for(let i = 1; i <= 100; i++){
+    let value = ((i % fizzValue == 0 ? 'Fizz' : '') + (i % buzzValue == 0 ? 'Buzz' : '' ) || i);
+    returnArray.push(value);
+  }
+
+  return returnArray;
+}
 
 // view
 function display(fbArray) {
@@ -46,17 +93,28 @@ function display(fbArray) {
   tableBody.innerHTML = "";
 
   for (let i = 0; i < fbArray.length; i += 5) {
+    // grabs content inside the td and tr tags in the app.html
     let tableRow = document.importNode(templateRow.content, true);
 
+    // make an array of all the td tags
     let rowCols = tableRow.querySelectorAll("td");
+
+    // grabs the first td tag and sets text content to i
+    rowCols[0].classList.add(fbArray[i]);
     rowCols[0].textContent = fbArray[i];
+
+    rowCols[1].classList.add(fbArray[i + 1]);
     rowCols[1].textContent = fbArray[i + 1];
+
+    rowCols[2].classList.add(fbArray[i + 2]);
     rowCols[2].textContent = fbArray[i + 2];
+
+    rowCols[3].classList.add(fbArray[i + 3]);
     rowCols[3].textContent = fbArray[i + 3];
+
+    rowCols[4].classList.add(fbArray[i + 4]);
     rowCols[4].textContent = fbArray[i + 4];
 
     tableBody.appendChild(tableRow);
   }
 }
-
-// watch video from the top to get names of IDs
